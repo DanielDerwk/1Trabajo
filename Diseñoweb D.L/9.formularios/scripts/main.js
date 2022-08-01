5f34e1563bfdf6cb8a40c3c3e82fddadd0ea4e46
DanielDerwk <109539974+DanielDerwk@users.noreply.github.com>
2022-08-01 11:09:56 -0500

    Add files via upload

---
+++ Diseñoweb D.L/9.formularios/scripts/main.js
@@ -0,0 +1,10 @@
+'use strict';
+const btn = document.getElementById('boton2');
+
+btn.addEventListener('click', (e) =>{//2do parametro funcion tipo flecha anonima
+    event.preventDefault();
+    const form = document.getElementById('form');
+    console.log(form);
+    console.log(form[0].value);
+    console.log(form[1].value);
+});
